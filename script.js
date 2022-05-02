cc8590d078d7e9e41373a9b74be0f745b7f8deb5
Kaung Pyae Min Thein <kaungpyae.kg@gmail.com>
2022-05-02 12:29:24 +0630

    digital clock

--- script.js
+++ script.js
@@ -1,56 +1,16 @@
-const addForm = document.querySelector('.addForm');
-const todos = document.querySelector('.todos');
-const search = document.querySelector('.search input');
+const clock = document.querySelector('.clock');
 
-//generate html list
-const todoList = todo => {
-  const HTML = `
-  <li class="list-group-item d-flex align-items-center justify-content-between">
-    <span>${todo}</span>
-    <i class="fa-solid fa-trash-can delete"></i>
-  </li>`;
 
-  todos.innerHTML += HTML;
 
+const tick = _ => {
+  const now = new Date();
+  const hour = now.getHours();
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+  const Html = `<span>${hour}</span> :
+                <span>${min}</span> : 
+                <span>${sec}</span>`;
+  clock.innerHTML = Html;
 };
 
-//add todo
-addForm.addEventListener('submit', e => {
-  e.preventDefault();
-  let todo = addForm.add.value.trim();
-
-  if (todo.length) {
-    todoList(todo);
-    addForm.reset();
-  }
-
-});
-
-//delete event
-todos.addEventListener('click', e => {
-  if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove();
-  }
-
-});
-
-//filter and add & delete class
-const filterList = input => {
-
-  Array.from(todos.children)
-    .filter(todo => !todo.textContent.toLowerCase().includes(input))
-    .forEach(todo => todo.classList.add('filter'));
-
-  Array.from(todos.children)
-    .filter(todo => todo.textContent.toLowerCase().includes(input))
-    .forEach(todo => todo.classList.remove('filter'));
-
-};
-
-//search keyup
-search.addEventListener('keyup', _ => {
-  console.log(search.value);
-  const input = search.value.trim().toLowerCase();
-  filterList(input);
-
-});
+setInterval(tick, 1000);
