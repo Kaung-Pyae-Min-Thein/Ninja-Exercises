4e649e08f000a4f036c82550691593923b5a5309
Kaung Pyae Min Thein <kaungpyae.kg@gmail.com>
2022-05-04 14:53:59 +0630

    asyn

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const clock = document.querySelector('.clock');
+const getTodos = (resource, callback) => {
+  const HttpReq = new XMLHttpRequest();
 
-
-
-const tick = _ => {
-  const now = new Date();
-  const hour = now.getHours();
-  const min = now.getMinutes();
-  const sec = now.getSeconds();
-  const Html = `<span>${hour}</span> :
-                <span>${min}</span> : 
-                <span>${sec}</span>`;
-  clock.innerHTML = Html;
+  HttpReq.addEventListener("readystatechange", () => {
+    if (HttpReq.status === 200 && HttpReq.readyState === 4) {
+      const data = JSON.parse(HttpReq.responseText);
+      callback(undefined, data);
+    }
+    else if (HttpReq.readyState === 4) {
+      callback("could not fetch data", undefined);
+    }
+  });
+  HttpReq.open("GET", resource);
+  HttpReq.send();
 };
 
-setInterval(tick, 1000);
+getTodos("todos/kaung.json", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log(data);
+  }
+  
+
+});
+
+
+ // req.open("GET", 'https://jsonplaceholder.typicode.com/todos/');
