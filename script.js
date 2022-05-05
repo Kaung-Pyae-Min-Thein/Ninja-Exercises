c49c175b8a9d0fd7db1908e76c33aa7cd9fb44c0
Kaung Pyae Min Thein <kaungpyae.kg@gmail.com>
2022-05-05 15:50:38 +0630

    promise

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-const getTodos = (resource, callback) => {
-  const HttpReq = new XMLHttpRequest();
+const getTodos = (resource) => {
 
-  HttpReq.addEventListener("readystatechange", () => {
-    if (HttpReq.status === 200 && HttpReq.readyState === 4) {
-      const data = JSON.parse(HttpReq.responseText);
-      callback(undefined, data);
-    }
-    else if (HttpReq.readyState === 4) {
-      callback("could not fetch data", undefined);
-    }
+  return new Promise((resolve, reject) => {
+    const HttpReq = new XMLHttpRequest();
+
+    HttpReq.open("GET", resource);
+    HttpReq.send();
+
+    HttpReq.addEventListener('readystatechange', () => {
+      if (HttpReq.status === 200 && HttpReq.readyState === 4) {
+        const data = JSON.parse(HttpReq.responseText);
+        resolve(data);
+      }
+      else if (HttpReq.readyState === 4) {
+        reject("Error in fetching data");
+      }
+    });
   });
-  HttpReq.open("GET", resource);
-  HttpReq.send();
 };
 
-getTodos("todos/kaung.json", (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    console.log(data);
-  }
-  
+getTodos("todos/kaung.json")
+  .then(data => {
 
-});
+    console.log("first resolve", data);
+    return getTodos('todos/luigi.json');
+  })
+  .then(data => {
+    console.log("second resolve", data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 
  // req.open("GET", 'https://jsonplaceholder.typicode.com/todos/');
